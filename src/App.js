4b9027fc6515cff7b2d8639f296f0f10baaf10d2
LeeYooBin <santosroberdan25@gmail.com>
2023-09-28 04:07:34 -0300

    Adjusting GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
-import Main from './components/Main';
+import Home from './pages/Home';
+import { createGlobalStyle } from 'styled-components';
 
-export default function App(){
-  return(
-    <>
-      <Main />
-    </>   
-  );
-}
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: Kumbh Sans;
+
+    ::-webkit-scrollbar {
+      width: .5rem;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: #000000;
+    }
+  }
+`;
+
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <Home />
+  </>
+)
+
+export default App;
