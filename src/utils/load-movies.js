016f3aebf22d71eae6314e8f165f4c91dc3428c9
LeeYooBin <santosroberdan25@gmail.com>
2023-09-28 04:10:00 -0300

    Adding .env and making adjusts

--- src/utils/load-movies.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const moviesApi = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/movie/popular?api_key=13f2a9b970b1366ea5911d705a1cffd3'
-});
-
-export default moviesApi;
